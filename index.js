b3d550a115a85d98bacaa77149e5ff73e299ac78
jahidanowar <milon5143@gmail.com>
2021-11-10 07:15:26 +0530

    1.0.3

--- index.js
+++ index.js
@@ -2,44 +2,40 @@ import request from "request";
 
 class Recaptcha {
   constructor(secret, minScore) {
+    console.log("secret", secret, "minScore", minScore);
+    if (!secret) {
+      throw new Error("No secret provided");
+    }
     this.secret = secret;
     this.minScore = minScore || 0.5;
   }
 
-  async verify(req, res, next) {
-    const { token } = req.body;
-
-    if (!token) {
-      return next(() => {
+  verify = () => {
+    return (req, res, next) => {
+      const { token } = req.body;
+      if (!token) {
         throw new Error("No token provided");
-      });
-    }
+      }
+      const url = `https://www.google.com/recaptcha/api/siteverify?secret=${this.secret}&response=${token}`;
 
-    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${this.secret}&response=${token}`;
-    request(url, (err, response, body) => {
-      if (err) {
-        return next(() => {
+      request(url, (err, response, body) => {
+        if (err) {
           throw new Error("Error verifying recaptcha");
-        });
-      }
+        }
+        const { success, score } = JSON.parse(body);
 
-      const { success, score } = JSON.parse(body);
+        console.log("success", success, "score", score);
 
-      if (!success) {
-        return next(() => {
+        if (!success) {
           throw new Error("Recaptcha verification failed");
-        });
-      }
-
-      if (score < this.minScore) {
-        return next(() => {
+        }
+        if (score < this.minScore) {
           throw new Error("Recaptcha verification score too low");
-        });
-      }
-
-      next();
-    });
-  }
+        }
+        next();
+      });
+    };
+  };
 }
 
 export default Recaptcha;
